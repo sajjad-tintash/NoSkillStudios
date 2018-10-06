2e511f1ebd487df2c5e2ffdfbd076d095a2957f7
Ali Mustafa <ali.mustafa@tintash.com>
2018-10-06 16:53:11 +0500

    First commit.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform Target;
+
+	public Vector2 
+		Margin,
+		Smoothing;
+
+	public BoxCollider2D Bounds;
+
+	public bool FollowingX { get; set;}
+	public bool FollowingY { get; set;}
+
+	private Vector3 
+	_min,
+	_max;
+	
+	private Camera _mainCamera;
+
+	//public static CameraController instance = null;
+
+	void Awake () {
+        //if (instance == null)
+        //{
+        //	instance = this;
+        //}
+        //else Destroy (this.gameObject);
+
+        //this.enabled = false;
+
+        FollowingX = true;
+        FollowingY = true;
+	}
+
+	public void Init ()
+	{
+		_mainCamera = Camera.main;
+		
+		_min = Bounds.bounds.min;
+		_max = Bounds.bounds.max;
+
+		this.enabled = true;
+	}
+
+	void Update ()
+	{
+		if (FollowingX || FollowingY) 
+		{	
+			float x = transform.position.x;
+			float y = transform.position.y;
+			float cameraWidth = _mainCamera.orthographicSize * ((float)Screen.width / Screen.height);
+
+			if (FollowingX) {
+				if (Mathf.Abs (x - Target.position.x) > Margin.x) {
+					x = Mathf.Lerp (x, Target.position.x, Smoothing.x * Time.deltaTime);
+				}
+
+				x = Mathf.Clamp (x, _min.x + cameraWidth, _max.x - cameraWidth);
+			}
+
+			if (FollowingY) {
+				if (Mathf.Abs (y - Target.position.y) > Margin.y) {
+					y = Mathf.Lerp (y, Target.position.y, Smoothing.y * Time.deltaTime);
+				}
+
+				y = Mathf.Clamp (y, _min.y + _mainCamera.orthographicSize, _max.y - _mainCamera.orthographicSize);
+			}
+
+			transform.position = new Vector3 (x, y, transform.position.z);
+		}
+	}
+
+	public void ResetCamera ()
+	{
+		FollowingX = false;
+		FollowingY = false;
+
+		transform.position = new Vector3 (0f, 0f, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class GameManager : MonoBehaviour {
+
+    public enum World
+    {
+        NORMAL,
+        MUTATED
+    }
+
+    public PlayerPlatformerController _normalPlayerController;
+    public PlayerPlatformerController _mutatedPlayerController;
+
+    public PostProcessingBehaviour _normalCameraPostProcessing;
+    public PostProcessingBehaviour _mutatedCameraPostProcessing;
+
+    public CameraController _normalCameraController;
+    public CameraController _mutatedCameraController;
+
+    private PostProcessingProfile _normalCameraPostProcessingProfile;
+    private PostProcessingProfile _mutatedCameraPostProcessingProfile;
+
+    private World _currentWorld;
+
+    private void Awake()
+    {
+        _normalCameraPostProcessingProfile = _normalCameraPostProcessing.profile;
+        _mutatedCameraPostProcessingProfile = _mutatedCameraPostProcessing.profile;
+    }
+
+    // Use this for initialization
+    void Start () {
+        _currentWorld = ActivateWorld(World.NORMAL);
+
+        _normalCameraController.Init();
+        _mutatedCameraController.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            switch(_currentWorld)
+            {
+                case World.MUTATED:
+                    _currentWorld = ActivateWorld(World.NORMAL);
+                    break;
+
+                case World.NORMAL:
+                    _currentWorld = ActivateWorld(World.MUTATED);
+                    break;
+            }
+        }
+	}
+
+    private World ActivateWorld (World world)
+    {
+        switch (world)
+        {
+            case World.MUTATED:
+                _normalPlayerController.enabled = false;
+                _mutatedPlayerController.enabled = true;
+                MakeWorldGreyScale(World.NORMAL);
+                break;
+
+            case World.NORMAL:
+                _normalPlayerController.enabled = true;
+                _mutatedPlayerController.enabled = false;
+                MakeWorldGreyScale(World.MUTATED);
+                break;
+        }
+
+        MakeWorldColored(world);
+
+        return world;
+    }
+
+    private void MakeWorldGreyScale (World world)
+    {
+        ColorGradingModel.Settings settings;
+
+        switch (world)
+        {
+            case World.MUTATED:
+                settings = _mutatedCameraPostProcessingProfile.colorGrading.settings;
+                settings.basic.saturation = 0f;
+                _mutatedCameraPostProcessingProfile.colorGrading.settings = settings;
+                break;
+
+            case World.NORMAL:
+                settings = _normalCameraPostProcessingProfile.colorGrading.settings;
+                settings.basic.saturation = 0f;
+                _normalCameraPostProcessingProfile.colorGrading.settings = settings;
+                break;
+        }
+    }
+
+    private void MakeWorldColored(World world)
+    {
+        ColorGradingModel.Settings settings;
+
+        switch (world)
+        {
+            case World.MUTATED:
+                settings = _mutatedCameraPostProcessingProfile.colorGrading.settings;
+                settings.basic.saturation = 1f;
+                _mutatedCameraPostProcessingProfile.colorGrading.settings = settings;
+                break;
+
+            case World.NORMAL:
+                settings = _normalCameraPostProcessingProfile.colorGrading.settings;
+                settings.basic.saturation = 1f;
+                _normalCameraPostProcessingProfile.colorGrading.settings = settings;
+                break;
+        }
+    }
+}
